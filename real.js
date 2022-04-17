8b414988e62733a1be6494d6c46ef28888704dc4
Knowledge lovers <kyanmahajan676@gmail.com>
2022-04-17 14:27:09 +0530

    Initial commit

---
+++ real.js
@@ -0,0 +1,4 @@
+const fs = require("fs");
+const text = fs.readFileSync("jj.html", "utf-8");
+console.log(text);
+ 
---
+++ xx.js
@@ -0,0 +1,16 @@
+console.log("hi");
+
+let name = "harry";
+
+function greet(name){
+    console.log(name + "hi");
+
+}
+greet(name);
+ 
+let name = prompt("what is your name", "guest");
+console.log(name);
+let i = 0;
+for(i = 0; i<3; i++){
+    console.log(i);
+}
